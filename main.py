69c45e5e7a1a42609a886e546ff9ecaa4a7bd1c4
wyw <237316090@qq.com>
2023-12-14 23:21:03 +0800

    1214

--- main.py
+++ main.py
@@ -29,6 +29,15 @@ from tensorflow.keras.layers import Dense, Dropout
 from tensorflow import keras
 from tensorflow.keras.callbacks import LambdaCallback
 # @colorful('blueDeep')
+import predict as pre
+import pymc3 as pm
+import theano
+import theano.tensor as T
+import matplotlib
+import arviz as az
+from collections import Counter
+
+
 
 class main_win_Ui(QWidget, ML_win.Ui_Dialog):
     def __init__(self):
@@ -46,7 +55,6 @@ class ml_Ui(QWidget, ml.Ui_ML_Dialog):
         super(ml_Ui, self).__init__()
         self.setupUi(self)
 
-
 if __name__ == '__main__':
 
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
@@ -60,6 +68,7 @@ if __name__ == '__main__':
     dialog = login.LoginWindow()
     # dialog.setWindowIcon(QIcon('logo.ico'))
     MainWindow = QtWidgets.QWidget()
+
 
     def open_url():
         # 在这里添加你要打开的 URL
@@ -133,6 +142,7 @@ if __name__ == '__main__':
         global x_test
         global y_train
         global y_test
+        global goal_col
         try:
             def minmax_norm(df_input):
                 return (df_input - df_input.min()) / (df_input.max() - df_input.min())
@@ -187,7 +197,6 @@ if __name__ == '__main__':
         except Exception as e:
             print('错误信息:', e)
 
-
     def draw_classify(train_predict, test_predict, y_train, y_test, name):
         fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(8, 6))  # 创建两个子图，纵向排列，指定画布大小
         # 绘制第一个子图 train
@@ -220,9 +229,10 @@ if __name__ == '__main__':
 
         plt.show()
 
+
     def SR_train():
-        global model_dict
         try:
+            global model_dict
             epochs, c = ml.spinBox_SR_epochs.value(), ml.spinBox_c.value()
             softmax_reg = LogisticRegression(multi_class="multinomial",
                                              solver="lbfgs",  # 求解器
@@ -299,6 +309,301 @@ if __name__ == '__main__':
             ml.label_ANN.setText(f"训练发生错误，请重试！")
             print('错误信息:', e)
 
+    def show_prediction_window():
+        try:
+            global prediction_window
+            x_test_columns = x_test.columns
+            prediction_window = pre.PredictionWindow(x_test_columns)
+            prediction_window.predict_button.clicked.connect(perform_prediction)
+            prediction_window.show()
+        except Exception as e:
+            ml.label_predict.setText(f"训练发生错误，请重试！")
+            print('错误信息:', e)
+
+    def perform_prediction():
+        try:
+            if ml.comboBox_model.currentText() == 'SoftMax回归':
+                model_name = 'SR_model'
+            elif ml.comboBox_model.currentText() == '深度神经网络':
+                model_name = 'ANN_model'
+            elif ml.comboBox_model.currentText() == '贝叶斯SoftMax回归':
+                model_name = 'ANN_model'
+            else:
+                model_name = 'BNN_model'
+            input_values = [[float(entry.text()) for entry in prediction_window.entry_fields]]
+            # 在此处添加模型预测的代码
+            model = model_dict[model_name]
+            res = model.predict(input_values)[0]
+            if res in [0, 1]:
+                type = '软岩'
+            elif res in [2, 3]:
+                type = '较硬岩'
+            else:
+                type = '硬岩'
+            # 替换上面的注释行为实际的模型预测代码
+            prediction_window.label_res.setText(f'{ml.comboBox_model.currentText()}预测结果：当前{goal_col}为{res},属于{type}')
+            print(res)
+        except Exception as e:
+            prediction_window.label_res.setText('输入有误')
+            print('错误信息:', e)
+
+    def predict_BYS(X, y, model, trace, sample=100):
+        #     y = pd.Categorical(y).codes
+        ann_input.set_value(np.array(X))
+        ppc = pm.sample_posterior_predictive(trace, model=model, samples=sample)
+        mark = [0, 1, 2, 3, 4, 5]
+        result = [Counter(ppc['out'][:, i]) for i in range(ppc['out'].shape[1])]
+        # pred = stats.mode(ppc['out'], axis=0)[0][0]
+        res = []
+        for i in result:
+            ls = []
+            for n in mark:
+                ls.append(i[n] / sample)
+            res.append(ls)
+
+        Prob = pd.DataFrame(res, columns=mark)  # 概率表
+        pred = np.argmax(res, axis=1)  # 概率最大类别
+        # pred_p = np.max(res, axis=1)
+        acc = np.sum(y == pred, axis=0) / len(y)  # 准确率
+
+        diff = 0  # prob个数
+        prob = np.argsort(res, axis=1)[:, -2]  # 概率第二大
+        prob_p = np.sort(res, axis=1)[:, -2]
+        for i in range(len(prob)):
+            if prob_p[i] < 0.3:  # 设置阈值为0.3
+                prob[i] = pred[i]
+            else:
+                diff += 1
+        c = 0  # 纠正个数
+        for i, j, k in zip(pred, prob, y):
+            if i != j and j == k:
+                c += 1
+
+        total_acc = (np.sum(y == pred, axis=0) + c) / len(y)
+        uncertain = diff / len(y)
+        redify = c / diff  # 纠正率
+        acc_conf = redify / (1 - acc)  # 不确定度有效性
+
+        print("acc=", acc)
+        print("total_acc=", total_acc)
+        print("uncertain=", uncertain)
+        print("redify=", redify)
+        print("acc_conf=", acc_conf)
+        return pred, prob, Prob, acc, total_acc, uncertain, redify, acc_conf
+
+    def draw_BYS(predict_train, predict_test, prob_train, prob_test, y_train, y_test, name):
+        fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(8, 6))  # 创建两个子图，纵向排列，指定画布大小
+        # 绘制第一个子图 train
+        n_train = min(100, len(y_train))
+        indices_train = np.arange(n_train)
+        # train_diff_indices = np.where(predict_train[:n_train] != y_train[:n_train])[0]
+        correct_train_indices = \
+        np.where((y_train[:n_train] == prob_train[:n_train]) & (y_train[:n_train] != predict_train[:n_train]))[0]
+        correct_train_x = indices_train[correct_train_indices]
+        correct_train_y = prob_train[correct_train_indices]
+
+        incorrect_train_indices = \
+        np.where((y_train[:n_train] != prob_train[:n_train]) & (y_train[:n_train] != predict_train[:n_train]))[0]
+        incorrect_train_x = indices_train[incorrect_train_indices]
+        incorrect_train_y = y_train.reset_index(drop=True)[incorrect_train_indices]
+
+
+        axes[0].scatter(indices_train, prob_train[:n_train], s=80, alpha=0.5, label='prob_train', marker='o')
+        axes[0].scatter(indices_train, predict_test[:n_train], s=80, label='predict_train', marker='o')
+        axes[0].scatter(indices_train, y_train[:n_train], s=30, label='y_train', marker='o')
+
+        axes[0].scatter(correct_train_x, correct_train_y, s=150, label='redify Prediction', facecolors='none',
+                        edgecolors='red', marker='o')
+        axes[0].scatter(incorrect_train_x, incorrect_train_y, s=150, marker='x', color='red',
+                        label='Incorrect Prediction')
+        # if len(train_diff_indices) > 0:
+        #     axes[0].scatter(train_diff_indices, predict_train[train_diff_indices], s=150, marker='x', color='red',
+        #                     label='Incorrect Prediction')
+        axes[0].legend()
+        axes[0].set_title('Train')
+
+        # 绘制第二个子图 test
+        n_test = min(100, len(y_test))
+        indices_test = np.arange(n_test)
+        # test_diff_indices = np.where(predict_test[:n_test] != y_test[:n_test])[0]
+        correct_test_indices = \
+        np.where((y_test[:n_test] == prob_test[:n_test]) & (y_test[:n_test] != predict_test[:n_test]))[0]
+        correct_test_x = indices_test[correct_test_indices]
+        correct_test_y = prob_test[correct_test_indices]
+
+        incorrect_test_indices = \
+        np.where((y_test[:n_test] != prob_test[:n_test]) & (y_test[:n_test] != predict_test[:n_test]))[0]
+        incorrect_test_x = indices_test[incorrect_test_indices]
+        incorrect_test_y = y_test.reset_index(drop=True)[incorrect_test_indices]
+
+
+        axes[1].scatter(indices_test, prob_test[:n_test], s=80, alpha=0.5, label='prob_test', marker='o')
+        axes[1].scatter(indices_test, predict_test[:n_test], s=80, label='predict_test', marker='o')
+        axes[1].scatter(indices_test, y_test[:n_test], s=30, label='y_test', marker='o')
+
+        axes[1].scatter(correct_test_x, correct_test_y, s=150, label='redify Prediction', facecolors='none',
+                        edgecolors='red', marker='o')
+        axes[1].scatter(incorrect_test_x, incorrect_test_y, s=150, marker='x', color='red',
+                        label='Incorrect Prediction')
+        # if len(test_diff_indices) > 0:
+        #     axes[1].scatter(test_diff_indices, predict_test[test_diff_indices], s=150, marker='x', color='red',
+        #                     label='Incorrect Prediction')
+        axes[1].legend()
+        axes[1].set_title('Test')
+
+        plt.tight_layout()
+        plt.suptitle(name)
+        plt.subplots_adjust(top=0.9, bottom=0.1, left=0.1, right=0.9, hspace=0.4, wspace=0.4)
+
+        plt.show()
+
+    def BSR_train():
+        try:
+            global model_dict
+            global ann_input
+            global BSR_trace
+            ml.label_BSR.setText("训练中...")  # 需要多线程才能展示
+            epochs = ml.spinBox_BSR_epochs.value()
+            ann_input = theano.shared(np.array(x_train))  # 设置为共享权重
+            ann_output = theano.shared(y_train.values)
+            types = len(set(pd.Categorical(y_train).codes))
+            with pm.Model() as BSR_model:
+                alpha = pm.Normal('alpha', mu=0, sd=2, shape=types)  # shape=（类别数）
+                beta = pm.Normal('beta', mu=0, sd=2, shape=(x_train.shape[1], types))  # shape=（参数个数，类别数）
+                mu = alpha + pm.math.dot(ann_input, beta)
+                theta = T.nnet.softmax(mu)
+                out = pm.Categorical('out', p=theta, observed=ann_output)
+                if ml.comboBox_BSR.currentText() == '马尔可夫蒙特卡洛法':# BSR_MC
+                    start = pm.find_MAP()
+                    step = pm.NUTS()# 定义采样方法（NUTS适用于连续变量）
+                    trace_BSRMC = pm.sample(epochs, step, start)
+                    BSR_trace = trace_BSRMC
+                else:  # BSR_VI
+                    inference = pm.ADVI()
+                    approx = pm.fit(n=epochs, method=inference, obj_optimizer=pm.adam(learning_rate=0.01))
+                    trace_BSRVI = pm.variational.sample_approx(approx, draws=2000)
+                    BSR_trace = trace_BSRVI
+            model_dict['BSR_model'] = BSR_model
+            ml.label_BSR.setText("训练完成！")
+
+            # 采样迹线
+            plt.rc('font', family='Times New Roman')
+            matplotlib.rc('xtick', labelsize=12)
+            # plt.style.use('classic')
+            az.plot_trace(trace_BSRVI)
+            # plt.savefig("D:/Desktop/大论文/图表和结果/导出图片/BSR_VI_采样.png", format="png", dpi=600)
+            plt.tight_layout()
+            plt.show()
+
+
+        except:
+            ml.label_BSR.setText("训练发生错误，请重试！")
+
+    def BSR_res():
+        try:
+            sampling = ml.spinBox_BSR_sampling.value()
+            model = model_dict['BSR_model']
+            trace = BSR_trace
+            predict_train, prob_train, Prob_train, acc, total_acc, uncertain, redify, acc_conf = predict_BYS(X=x_train, y=y_train, model=model, trace=trace, sample=sampling)
+            predict_test, prob_test, Prob_test, acc, total_acc, uncertain, redify, acc_conf = predict_BYS(X=x_test, y=y_test, model=model, trace=trace, sample=sampling)
+            draw_BYS(predict_train=predict_train, predict_test=predict_test, prob_train=prob_train, prob_test=prob_test,
+                     y_train=y_train, y_test=y_test, name='BSR')
+            ml.label_BSR.setText(f"准确率:{round(acc,2)},总准确率:{round(total_acc)}\n不确定度:{round(uncertain,2)},纠正率:{round(redify,2)}\n不确定度有效性:{round(acc_conf,2)}")
+        except:
+            ml.label_BSR.setText("训练发生错误，请重试！")
+
+    def BNN_train():
+        try:
+            global model_dict
+            global ann_input
+            global BNN_trace
+            epochs = ml.spinBox_BNN_epochs.value()
+            layer = ml.spinBox_BNN_layers.value()
+            n_hidden = ml.spinBox_BNN_nums.value()
+
+            ann_input = theano.shared(np.array(x_train))  # 设置为共享权重
+            ann_output = theano.shared(pd.Categorical(y_train).codes)
+            types = len(set(pd.Categorical(y_train).codes))
+            # BNN
+            n_hidden = 30
+            init_1 = np.random.randn(x_train.shape[1], n_hidden)
+            init_2 = np.random.randn(n_hidden, n_hidden)
+            init_3 = np.random.randn(n_hidden, n_hidden)
+            init_out = np.random.randn(n_hidden, types)
+
+            with pm.Model() as BNN_model:
+                # Weights from input to hidden layer
+                weights_in_1 = pm.Normal('w_in_1', 0, sd=2,
+                                         shape=(x_train.shape[1], n_hidden),
+                                         testval=init_1)
+                b0 = pm.Normal('b0', mu=0, sd=2, shape=n_hidden)
+
+                # Weights from 1st to 2nd layer
+                weights_1_2 = pm.Normal('w_1_2', 0, sd=2,
+                                        shape=(n_hidden, n_hidden),
+                                        testval=init_2)
+                b1 = pm.Normal('b1', mu=0, sd=2, shape=n_hidden)
+
+                # Weights from hidden layer to output
+                weights_2_out = pm.Normal('w_2_out', 0, sd=1,
+                                          shape=(n_hidden, types),
+                                          testval=init_out)
+                b = pm.Normal('b', mu=0, sd=2, shape=types)
+
+                # Build neural-network using tanh activation function
+                act_1 = T.tanh(T.dot(ann_input, weights_in_1) + b0)
+                act_2 = T.tanh(T.dot(act_1, weights_1_2) + b1)
+                act_out = T.nnet.softmax(T.dot(act_2, weights_2_out) + b)
+
+                # Binary classification -> Bernoulli likelihood
+                out = pm.Categorical('out', p=act_out, observed=ann_output)
+
+                if ml.comboBox_BSR.currentText() == '马尔可夫蒙特卡洛法':  # BNN_MC
+                    start = pm.find_MAP()
+                    step = pm.NUTS()# 定义采样方法（NUTS适用于连续变量）
+                    trace_BNN = pm.sample(2000, step, start)
+
+                else:  # BNN_VI
+                    inference = pm.ADVI()
+                    approx = pm.fit(n=2000, method=inference, obj_optimizer=pm.adam(learning_rate=0.01))
+                    trace_BNN = pm.variational.sample_approx(approx, draws=2000)
+
+            model_dict['BNN_model'] = BNN_model
+            ml.label_BNN.setText("训练完成！")
+
+            # 采样迹线
+            plt.rc('font', family='Times New Roman')
+            matplotlib.rc('xtick', labelsize=12)
+            az.plot_trace(trace_BNN)
+            # plt.savefig("D:/Desktop/大论文/图表和结果/导出图片/BSR_VI_采样.png", format="png", dpi=600)
+            plt.tight_layout()
+            plt.show()
+
+
+        except:
+            ml.label_BSR.setText("训练发生错误，请重试！")
+
+    def BNN_res():
+        try:
+            sampling = ml.spinBox_BSR_sampling.value()
+            model = model_dict['BSR_model']
+            trace = BSR_trace
+            predict_train, prob_train, Prob_train, acc, total_acc, uncertain, redify, acc_conf = predict_BYS(X=x_train,
+                                                                                                             y=y_train,
+                                                                                                             model=model,
+                                                                                                             trace=trace,
+                                                                                                             sample=sampling)
+            predict_test, prob_test, Prob_test, acc, total_acc, uncertain, redify, acc_conf = predict_BYS(X=x_test,
+                                                                                                          y=y_test,
+                                                                                                          model=model,
+                                                                                                          trace=trace,
+                                                                                                          sample=sampling)
+            draw_BYS(predict_train=predict_train, predict_test=predict_test, prob_train=prob_train, prob_test=prob_test,
+                     y_train=y_train, y_test=y_test, name='BSR')
+            ml.label_BSR.setText(
+                f"准确率:{round(acc, 2)},总准确率:{round(total_acc)}\n不确定度:{round(uncertain, 2)},纠正率:{round(redify, 2)}\n不确定度有效性:{round(acc_conf, 2)}")
+        except:
+            ml.label_BSR.setText("训练发生错误，请重试！")
 
     # if dialog.exec_() == QDialog.Accepted:
     if True:
@@ -341,13 +646,42 @@ if __name__ == '__main__':
             SR_res
         )
         ml.pushButton_SR_save.clicked.connect(
-            lambda: {ml.label_SR.setText('模型保存成功')}
+            lambda: {ml.label_SR.setText('保存成功')}
         )
+
         ml.pushButton_ANN_train.clicked.connect(
             ANN_train
         )
         ml.pushButton_ANN_res.clicked.connect(
             ANN_res
+        )
+        ml.pushButton_ANN_save.clicked.connect(
+            lambda: {ml.label_ANN.setText('保存成功')}
+        )
+
+        ml.pushButton_BSR_train.clicked.connect(
+            BSR_train
+        )
+        ml.pushButton_BSR_res.clicked.connect(
+            BSR_res
+        )
+        # ml.pushButton_SR_save.clicked.connect(
+        #     lambda: {ml.label_BSR.setText('保存成功')}
+        # )
+        #
+        # ml.pushButton_ANN_train.clicked.connect(
+        #     ANN_train
+        # )
+        # ml.pushButton_ANN_res.clicked.connect(
+        #     ANN_res
+        # )
+        # ml.pushButton_SR_save.clicked.connect(
+        #     lambda: {ml.label_ANN.setText('保存成功')}
+        # )
+
+
+        ml.pushButton_predict.clicked.connect(
+            show_prediction_window
         )
 
 
---
+++ predict.py
@@ -0,0 +1,73 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox
+
+class PredictionWindow(QWidget):
+    def __init__(self, x_test_columns):
+        super().__init__()
+        self.x_test_columns = x_test_columns
+        self.init_ui()
+
+    def init_ui(self):
+        layout = QVBoxLayout()
+
+        self.entry_fields = []
+        value_list = [0.21, 0.89, 0.05, 0.18, 0.12, 0.37, 0.32, 0.46, 0.03, 0.29]
+        for i,column in enumerate(self.x_test_columns):
+            label = QLabel(column)
+            layout.addWidget(label)
+            entry = QLineEdit()
+            entry.setText(str(value_list[i]))  # 设置默认值
+            layout.addWidget(entry)
+            self.entry_fields.append(entry)
+
+        self.predict_button = QPushButton('预测')
+        self.label_res = QLabel()
+        layout.addWidget(self.predict_button)
+        layout.addWidget(self.label_res)
+
+        # self.save_button = QPushButton('关闭')
+        # layout.addWidget(self.save_button)
+
+        self.setLayout(layout)
+        self.setWindowTitle('模型预测')
+
+    def perform_prediction(self):
+        try:
+            input_values = [[float(entry.text()) for entry in self.entry_fields]]
+
+            # 在此处添加模型预测的代码
+            # model.predict(input_values)
+            # 替换上面的注释行为实际的模型预测代码
+            QMessageBox.information(self, '预测结果', '这里是预测结果')  # 暂时用消息框代替预测结果
+        except Exception as e:
+            QMessageBox.critical(self, '错误', f'发生错误: {e}')
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        layout = QVBoxLayout()
+
+        self.predict_button = QPushButton('预测新窗口')
+        self.predict_button.clicked.connect(self.show_prediction_window)
+        layout.addWidget(self.predict_button)
+
+        self.setLayout(layout)
+        self.setWindowTitle('主界面')
+
+    def show_prediction_window(self):
+        x_test_columns = ['特征1', '特征2', '特征3']  # 替换为你实际的列名列表
+        self.prediction_window = PredictionWindow(x_test_columns)
+        self.prediction_window.show()
+
+def run_app():
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+# 运行主界面应用
+    run_app()
